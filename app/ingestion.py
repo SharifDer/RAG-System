05ceda006754630a9c54d0e266c2a05ffe8a80b4
SharifDer <sharifderhem@gmail.com>
2025-07-17 17:04:52 +0300

    updating ingestion

--- app/ingestion.py
+++ app/ingestion.py
@@ -11,7 +11,7 @@ COLLECTION_NAME = "support_issues"
 if os.path.exists(INDEX_DIR):
     shutil.rmtree(INDEX_DIR)
 def load_and_split_pdfs(pdf_dir):
-    splitter = RecursiveCharacterTextSplitter(separators=["\n\n"] , chunk_size = 5000 , chunk_overlap=50)
+    splitter = RecursiveCharacterTextSplitter(separators=["\n\n", "\n"] , chunk_size = 1000 , chunk_overlap=50)
     documents = []
     for file in os.listdir(pdf_dir):
         if file.lower().endswith(".pdf"):
@@ -25,7 +25,9 @@ def ingest_to_chroma(docs):
     client = chromadb.PersistentClient(path=INDEX_DIR)
     collection = client.get_or_create_collection(
                                                 name=COLLECTION_NAME 
-                                                ,embedding_function=embedding_function)
+                                                ,embedding_function=embedding_function
+                                                # ,metadata={"hnsw:space": "cosine"}
+                                                )
     for i, doc in enumerate(docs):
         collection.add(
             ids=[f"chunk_{i}"],
