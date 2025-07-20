346d1ddf39ba4a0ee8e9e3c7ae4d80b63ffa3952
SharifDer <sharifderhem@gmail.com>
2025-07-20 20:28:31 +0300

    update retrieval process

--- app/retrieval.py
+++ app/retrieval.py
@@ -2,6 +2,8 @@ from langchain_community.vectorstores.chroma import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain.schema import Document
 from typing import List
+import chromadb
+import time
 
 INDEX_DIR = "./data/faiss_index"
 COLLECTION_NAME = "support_issues"
@@ -14,10 +16,18 @@ embeddings = HuggingFaceEmbeddings(model_name=EMBEDDING_MODEL_NAME)
 vectordb = Chroma(
     collection_name=COLLECTION_NAME,
     embedding_function=embeddings,
-    persist_directory=INDEX_DIR
+    persist_directory=INDEX_DIR,
+    # client_settings=chromadb.Settings(anonymized_telemetry=False),
+    # collection_metadata={"hnsw:space": "cosine"}
 )
-
+print("metadatas",vectordb._collection.metadata)
 def retrieve_top_n(query:str , k:int =3) -> List[Document]:
+    start = time.time()
     query_embeddings = embeddings.embed_query(query)
-    results = vectordb.similarity_search_by_vector(query_embeddings, k=k)
+    results = vectordb.similarity_search_by_vector(
+        query_embeddings,
+        k=k,
+        )
+    retrieve_time = time.time() - start
+    print(f"retrieve time: {retrieve_time:.2f}s")
     return results
